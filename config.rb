a77180e560c7201e6e497e354dd5362910118c9d
Sandor Weisz <santheo@mac.com>
2013-11-13 17:14:14 -0600

    Added Sass/CSS configs.

---
+++ config.rb
@@ -0,0 +1,18 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = ""
+javascripts_dir = ""
+fonts_dir = ""
+
+output_style = :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+color_output = false
+preferred_syntax = :scss
